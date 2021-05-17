0f36520988cb74a25c4ff9905c30aa9e39e6550c
Mister-Three <81454774+Mister-Three@users.noreply.github.com>
2021-05-17 09:42:39 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 import webbrowser
 import time
+import math
 
 def add(n1, n2):
     print(int(n1) + int(n2))
@@ -9,12 +10,19 @@ def mult(n1, n2):
     print(int(n1) * int(n2))
 def div(n1, n2):
     print(int(n1) / int(n2))
+def remain(n1, n2):
+    print(int(n1) % int(n2))
+def power(n1, n2):
+    print(int(n1) ** int(n2))
+def pi():
+    print(math.pi)
+
 print("Welcome to M3r_\'s math problem solver!")
-print(" Addition, + \n Subtraction, - \n Division, / \n Multiplication, * ")
+print(" Addition, + \n Subtraction, - \n Division, / \n Multiplication, * \n Random \n Remainder \n Power, ** \n pi")
 while True:
     start = input("So, what type of math problem are we doing today? \n").lower()
     time.sleep(1)
-    if start in ['add', 'addition', 'Addition', 'sub', 'subtraction', 'Subtraction' 'mult', 'multiplication', 'Multiplication', 'div', 'division', 'Division', '*', '/', '-', '+']:
+    if start in ['add', 'addition', 'Addition', 'sub', 'subtraction', 'Subtraction', 'mult', 'multiplication', 'Multiplication', 'div', 'division', 'Division', '*', '/', '-', '+', 'remainder', 'power', '**', 'pi']:
         n1 = input("Input the first number: \n")
         n2 = input("Input the second number: \n")
         if start == 'add':
@@ -42,7 +50,7 @@ while True:
         elif start == 'Multiplication':
             mult(n1, n2)
         elif start == 'help':
-            print(" Addition, + \n Subtraction, - \n Division, / \n Multiplication, * \n Random")
+            print(" Addition, + \n Subtraction, - \n Division, / \n Multiplication, * \n Random \n Remainder \n Power, ** \n pi")
         elif start == '69':
             url = 'https://youtu.be/U2OjsKMcZQA'
             webbrowser.register('chrome',
@@ -51,7 +59,7 @@ while True:
             webbrowser.get('chrome').open(url)
             print("Enjoy fireball :D")
         elif start == 'Help':
-            print(" Addition, + \n Subtraction, - \n Division, / \n Multiplication, * \n Random")
+            print(" Addition, + \n Subtraction, - \n Division, / \n Multiplication, * \n Random \n Remainder \n Power, ** \n pi")
         elif start == '/':
             div(n1, n2)
         elif start == '*':
@@ -60,3 +68,15 @@ while True:
             add(n1, n2)
         elif start == '-':
             sub(n1, n2)
+        elif start == 'remainder':
+            remain(n1, n2)
+        elif start == 'Remainder':
+            remain(n1, n2)
+        elif start == 'power':
+            power(n1, n2)
+        elif start == 'Power':
+            power(n1, n2)
+        elif start == '**':
+            power(n1, n2)
+        elif start == 'pi':
+            pi()
